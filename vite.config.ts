59d4d942c0df94e8d8c0dd963aa166305087deed
DarkSigma-Labs <victor@lumithar.com>
2026-02-10 05:13:50 -0800

    Sitemap fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,25 @@ import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
 import { blogPosts } from './src/data/posts'
 
-const dynamicRoutes = Object.keys(blogPosts).map(slug => `/blog/${slug}`)
+const dynamicRoutes = [
+  ...Object.keys(blogPosts).map(slug => `/blog/${slug}`),
+  // Static Routes
+  '/services',
+  '/web-design',
+  '/local-seo',
+  '/local-marketing',
+  '/maintenance',
+  '/about',
+  '/contact',
+  '/work',
+  '/privacy',
+  '/terms',
+  '/sms',
+  '/app-development',
+  '/ai-automation',
+  '/seo-audit',
+  '/sitemap'
+]
 
 // https://vitejs.dev/config/
 export default defineConfig({
